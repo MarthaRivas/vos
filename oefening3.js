d196d6c33c53cda4228c6a77ee8d8e2469513234
MarthaRivas <groenezeeriv@gmail.com>
2018-08-28 11:23:14 +0200

    andere html voor interactie met browser

---
+++ oefening3.js
@@ -0,0 +1,14 @@
+workers = [
+    { name: "paul", alive: true, salary: 5000 },
+    { name: "rob", alive: false, salary: 3000},
+    { name: "anton", alive: false, salary: 5000},
+    { name: "steven", alive: true, salary: 2000 },
+    { name: "tom", alive: true, salary: 10000 },
+   ]
+   function distribute(workers) {
+       var totaalTeVerdelen = 0
+       var totaalAantalLevende =0
+       for ( var i = 1; i = workers.lengt; i = i +1)
+    // verdeel het salaris van de overleden werknemers
+    // over de levenden
+  }
