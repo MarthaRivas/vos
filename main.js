311380308eb6da4dd1c8f0e5cb252f28b4550d76
MarthaRivas <groenezeeriv@gmail.com>
2018-08-31 09:52:11 +0200

    voor werk, uitlegen en aanvragt van oefening

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
+function renderVos(vos) {
+  // document.createElement gebruiken om een element aan te maken
+  // dat elemt return
+}
+
+function renderVossen(vossen) {
+  // document.createElement gebruiken om een element aan te maken
+  // dat elemt return
+  var elem = document.createElement('')
+  return elem
+}
+
 function init() {
     Tabletop.init({
         key: 'https://docs.google.com/spreadsheets/d/1M7NapQMWRXoc_3RsvkK-egsFvCk3UaCsv7mXvvjSdaw/pubhtml',
-        
-
-      
       callback: function (tabletopData, tabletop) {
         console.log(tabletopData)
-   
-        var app4 = new Vue({
-          el: '#app-4',
-          data: {
-            list: tabletopData,
-          }
-        })
+
+        var elem = renderVossen(tabletopData)
+        document.body.appendChild(elem)
       },
       simpleSheet: true
     })
