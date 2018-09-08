56344476ea5f1c55c1be9ddbdc094afe669ac345
Martha Rivas <Tsubaki@MBP-van-Martha.lan>
2018-09-08 20:05:29 +0200

    step 1 render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-// var;render = require ( './render')
+var render = require ( './render')
 var tabletop = require ('tabletop') 
 
 tabletop.init({
         key: 'https://docs.google.com/spreadsheets/d/1M7NapQMWRXoc_3RsvkK-egsFvCk3UaCsv7mXvvjSdaw/pubhtml',
       callback: function(tabletopData, tabletop) {
+        console.log("render:", render)
         console.log(tabletopData, tabletop)
       
 
